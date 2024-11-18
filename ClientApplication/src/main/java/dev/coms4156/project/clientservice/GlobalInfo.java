40f44bfb71b9193a9426d24802e995a794b0b946
charlhhhh <clha826@gmail.com>
2024-11-18 15:55:59 -0500

    Can render index, but not create_donation

---
+++ ClientApplication/src/main/java/dev/coms4156/project/clientservice/GlobalInfo.java
@@ -0,0 +1,19 @@
+package dev.coms4156.project.clientservice;
+
+/**
+ * GlobalInfo holds constants and shared configuration for the client application.
+ */
+public class GlobalInfo {
+
+  // Base URL of the backend service
+  public static final String BASE_URL = "https://ase-team.ue.r.appspot.com";
+
+  // API Endpoints
+  public static final String CREATE_DONATION = "/createDonation";
+  public static final String RETRIEVE_AVAILABLE_ITEMS = "/retrieveAvailableItems";
+  public static final String DISPATCH_ITEMS = "/processRequests";
+
+  private GlobalInfo() {
+    // Prevent instantiation
+  }
+}
